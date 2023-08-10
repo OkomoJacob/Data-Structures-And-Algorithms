a4650e7222cc708a879885279ab0cc10c21a2310
OkomoJacob <okomo.code@gmail.com>
2023-08-10 22:40:29 +0300

    Arrays :fire:

---
+++ js/src/2. Data_Structures/reverseAString.js
@@ -0,0 +1,8 @@
+/*Reverse a string ([LeetCode 344](https://leetcode.com/problems/reverse-string/)).
+
+Given a string, like a sentence, it should reverse the individual characters.
+
+Example 1:
+Input: s = 'Hi My name is Jacob'
+Output: 'bocaJ si eman yM iH' */
+
