4a54243d003dd3222ec632ae364bbf602e73529d
OkomoJacob <okomo.code@gmail.com>
2023-08-11 20:46:38 +0300

    Reverse string using custom array approach

--- js/src/2. Data_Structures/reverseAString.js
+++ js/src/2. Data_Structures/reverseAString.js
@@ -6,3 +6,19 @@ Example 1:
 Input: s = 'Hi My name is Jacob'
 Output: 'bocaJ si eman yM iH' */
 
+let str = "Hi My name is Jacob";
+
+const reverseStringOptimized = (str) => {
+  if (!str || str.length < 2 || typeof str !== "string") {
+    return "Please recheck inputs";
+  }
+
+  const reversedString = [];
+  totalItems = str.length - 1;
+  for (let i = totalItems; i >= 0; i--) {
+    reversedString.push(str[i])
+  }
+  return reversedString.join('')
+};
+
+console.log("Reversed:", reverseStringBuiltIn(str));
